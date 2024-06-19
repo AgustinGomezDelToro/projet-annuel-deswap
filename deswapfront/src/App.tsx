00349a0d532334dda040c41481e5bcdb57d125a1
Agustin Gomez del toro <agustingomezdeltoro@MBP-de-Agustin.JOOWIN>
2024-06-19 11:02:49 +0200

    tokens - styles - app - utils integrated

---
+++ deswapfront/src/App.tsx
@@ -0,0 +1,62 @@
+import './styles/app.scss';
+import Header from './components/Header/Header';
+import { Route, Routes } from 'react-router-dom';
+import NotFound from './components/404/404';
+import Home from './components/Home/Home';
+import Swap from './components/Swap/Swap';
+import Tokens from './components/Tokens/Tokens';
+import Pools from './components/Pools/Pools';
+import Pool from './components/Pools/Pool/Pool';
+import { createWeb3Modal, defaultConfig } from '@web3modal/ethers/react'
+import NewPool from './components/Pools/Create/NewPool';
+
+function App() {
+
+  // 1. Get projectId at https://cloud.walletconnect.com
+  const projectId = 'e20faa0911a9c960827e06ea6f656510' // METTRE DANS LE .ENV
+
+  // 2. Set chains
+  const chains = [{
+    chainId: 11155111,
+    name: 'Sepolia',
+    currency: 'ETH',
+    explorerUrl: 'https://sepolia.etherscan.io',
+    rpcUrl: process.env.REACT_APP_RPC_URL!
+  }]
+
+  // 3. Create modal
+  const metadata = {
+    name: 'DeSwap',
+    description: 'DeSwap dapp',
+    url: '', // origin must match your domain & subdomain
+    icons: ['https://avatars.mywebsite.com/']
+  }
+
+  createWeb3Modal({
+    ethersConfig: defaultConfig({ metadata }),
+    chains,
+    projectId,
+    enableAnalytics: true, // Optional - defaults to your Cloud configuration
+    themeVariables: {
+      '--w3m-accent': "#EEEEEE",
+    },
+    themeMode: 'dark'
+  })
+
+  return (
+    <div className='min-h-screen bg'>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/swap" element={<Swap />} />
+        <Route path="/tokens" element={<Tokens />} />
+        <Route path="/pools" element={<Pools />} />
+        <Route path="/pools/:id" element={<Pool />} />
+        <Route path="/create" element={<NewPool />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ deswapfront/src/utils/hooks/connectWallet.tsx
@@ -0,0 +1,60 @@
+import { BrowserProvider, ethers } from 'ethers'
+import { useDisconnect, useWeb3Modal, useWeb3ModalAccount, useWeb3ModalProvider } from '@web3modal/ethers/react'
+import "../../styles/index.scss"
+import logo1 from "../../utils/asset/images/logo1.png";
+import { useEffect } from 'react'
+import User from '../../services/User'
+
+export default function ConnectButton() {
+    const { open } = useWeb3Modal()
+    const { address, isConnected } = useWeb3ModalAccount();
+    const { walletProvider } = useWeb3ModalProvider();
+    const { disconnect } = useDisconnect();
+
+    useEffect(() => {
+        async function signedMessage() {
+            if (!address) return;
+            const user = await new User().getOneByPublicKey(address);
+            if (user) return;
+            try {
+                const provider = new BrowserProvider(walletProvider as any)
+                const randomBytes = ethers.hexlify(ethers.randomBytes(16));
+                const now = new Date();
+                const message = `By signing this message, I allow the DeSwap App to save your public key.\n\nNonce: ${randomBytes} - ${now.toISOString()}`;
+                const signer = await provider.getSigner();
+                const signature = await signer?.signMessage(message);
+                if (signature) {
+                    try {
+                        const response = await new User().create(address, signature);
+                        if (response !== "User added successfully") {
+                            disconnect();
+                        }
+                    } catch (error) {
+                        disconnect();
+                        console.log(error)
+                    }
+                }
+            } catch (error) {
+                disconnect();
+                console.log(error)
+            }
+        }
+        if (isConnected) {
+            signedMessage();
+        }
+    }, [isConnected, address])
+
+    return (
+        <button className='text-white' onClick={() => open()}>
+            {
+                isConnected ?
+                    <span className='flex items-center gap-1 py-1.5 px-4'>
+                        <img className='h-8' src={logo1} alt='logo' />
+                        {address?.slice(0, 5)}...{address?.slice(address.length - 5)}
+                    </span>
+                    :
+                    <span className='flex items-center gap-1 py-3 px-6'>Connect wallet</span>
+            }
+        </button>
+    )
+}
