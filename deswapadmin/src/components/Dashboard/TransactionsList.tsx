fd3a23d2315520c815a7aae49c460b6486e18d6f
IgorLTS10 <igorchrzyptowicz@gmail.com>
2024-07-14 17:39:12 +0200

    Filtration for Transactions

--- deswapadmin/src/components/Dashboard/TransactionsList.tsx
+++ deswapadmin/src/components/Dashboard/TransactionsList.tsx
@@ -17,7 +17,9 @@ interface TransactionsListProps {
 
 const TransactionsList: React.FC<TransactionsListProps> = ({ address }) => {
     const [transactions, setTransactions] = useState<Transaction[]>([]);
+    const [filteredTransactions, setFilteredTransactions] = useState<Transaction[]>([]);
     const [currentPage, setCurrentPage] = useState(1);
+    const [filterQuery, setFilterQuery] = useState('');
     const transactionsPerPage = 10;
     const apiKey = process.env.REACT_APP_ETHERSCAN_API_KEY;
     const apiUrl = `https://api-sepolia.etherscan.io/api?module=account&action=txlist&address=${address}&startblock=0&endblock=99999999&sort=asc&apikey=${apiKey}`;
@@ -27,7 +29,9 @@ const TransactionsList: React.FC<TransactionsListProps> = ({ address }) => {
             try {
                 const response = await axios.get(apiUrl);
                 if (response.data.status === '1') {
-                    setTransactions(response.data.result.reverse());
+                    const fetchedTransactions = response.data.result.reverse();
+                    setTransactions(fetchedTransactions);
+                    setFilteredTransactions(fetchedTransactions);
                 } else {
                     console.error('Error fetching transactions:', response.data.message);
                 }
@@ -41,7 +45,7 @@ const TransactionsList: React.FC<TransactionsListProps> = ({ address }) => {
 
     const indexOfLastTransaction = currentPage * transactionsPerPage;
     const indexOfFirstTransaction = indexOfLastTransaction - transactionsPerPage;
-    const currentTransactions = transactions.slice(indexOfFirstTransaction, indexOfLastTransaction);
+    const currentTransactions = filteredTransactions.slice(indexOfFirstTransaction, indexOfLastTransaction);
 
     const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
 
@@ -58,36 +62,57 @@ const TransactionsList: React.FC<TransactionsListProps> = ({ address }) => {
         alert('Copied to clipboard: ' + text);
     };
 
+    const handleFilterChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const query = event.target.value.toLowerCase();
+        setFilterQuery(query);
+        if (query === '') {
+            setFilteredTransactions(transactions);
+        } else {
+            const filtered = transactions.filter(tx => tx.from.toLowerCase().includes(query));
+            setFilteredTransactions(filtered);
+        }
+        setCurrentPage(1);
+    };
+
     return (
         <div className="bg-white rounded-2xl shadow-lg p-8 overflow-x-auto">
             <h2 className="text-2xl font-semibold mb-4">Transactions List</h2>
+            <div className="w-full flex justify-end mb-4">
+                <input
+                    type="text"
+                    placeholder="Filter by 'From' address"
+                    value={filterQuery}
+                    onChange={handleFilterChange}
+                    className="w-full max-w-xs px-3 py-2 text-sm text-gray-700 placeholder-gray-500 bg-gray-100 border border-gray-400 rounded-lg focus:outline-none focus:ring-none"
+                />
+            </div>
             <table className="min-w-full bg-white">
                 <thead>
-                <tr>
-                    <th className="py-2 px-4 text-center text-lg">Transaction ID</th>
-                    <th className="py-2 px-4 text-center text-lg">Date</th>
-                    <th className="py-2 px-4 text-center text-lg">From</th>
-                    <th className="py-2 px-4 text-center text-lg">To</th>
-                    <th className="py-2 px-4 text-center text-lg">Amount</th>
-                    <th className="py-2 px-4 text-center text-lg">Status</th>
-                </tr>
+                    <tr>
+                        <th className="py-2 px-4 text-center text-lg">Transaction ID</th>
+                        <th className="py-2 px-4 text-center text-lg">Date</th>
+                        <th className="py-2 px-4 text-center text-lg">From</th>
+                        <th className="py-2 px-4 text-center text-lg">To</th>
+                        <th className="py-2 px-4 text-center text-lg">Amount</th>
+                        <th className="py-2 px-4 text-center text-lg">Status</th>
+                    </tr>
                 </thead>
                 <tbody>
-                {currentTransactions.map(tx => (
-                    <tr key={tx.hash} className="text-center text-base">
-                        <td className="py-2 px-4">
-                            <a
-                                href={`https://sepolia.etherscan.io/tx/${tx.hash}`}
-                                target="_blank"
-                                rel="noopener noreferrer"
-                                className="cursor-pointer"
-                                title="Click to view on Etherscan"
-                            >
-                                {abbreviateHash(tx.hash)}
-                            </a>
-                        </td>
-                        <td className="py-2 px-4">{new Date(parseInt(tx.timeStamp) * 10).toLocaleString()}</td>
-                        <td className="py-2 px-4">
+                    {currentTransactions.map(tx => (
+                        <tr key={tx.hash} className="text-center text-base">
+                            <td className="py-2 px-4">
+                                <a
+                                    href={`https://sepolia.etherscan.io/tx/${tx.hash}`}
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    className="cursor-pointer"
+                                    title="Click to view on Etherscan"
+                                >
+                                    {abbreviateHash(tx.hash)}
+                                </a>
+                            </td>
+                            <td className="py-2 px-4">{new Date(parseInt(tx.timeStamp) * 1000).toLocaleString()}</td>
+                            <td className="py-2 px-4">
                                 <span
                                     title={`Click to copy: ${tx.from}`}
                                     onClick={() => copyToClipboard(tx.from)}
@@ -95,8 +120,8 @@ const TransactionsList: React.FC<TransactionsListProps> = ({ address }) => {
                                 >
                                     {abbreviateAddress(tx.from)}
                                 </span>
-                        </td>
-                        <td className="py-2 px-4">
+                            </td>
+                            <td className="py-2 px-4">
                                 <span
                                     title={`Click to copy: ${tx.to}`}
                                     onClick={() => copyToClipboard(tx.to)}
@@ -104,16 +129,16 @@ const TransactionsList: React.FC<TransactionsListProps> = ({ address }) => {
                                 >
                                     {abbreviateAddress(tx.to)}
                                 </span>
-                        </td>
-                        <td className="py-2 px-4">{formatEther(tx.value)} ETH</td>
-                        <td className="py-2 px-4">{tx.isError === '0' ? 'Success' : 'Failed'}</td>
-                    </tr>
-                ))}
+                            </td>
+                            <td className="py-2 px-4">{formatEther(tx.value)} ETH</td>
+                            <td className="py-2 px-4">{tx.isError === '0' ? 'Success' : 'Failed'}</td>
+                        </tr>
+                    ))}
                 </tbody>
             </table>
             <Pagination
                 transactionsPerPage={transactionsPerPage}
-                totalTransactions={transactions.length}
+                totalTransactions={filteredTransactions.length}
                 paginate={paginate}
                 currentPage={currentPage}
             />
